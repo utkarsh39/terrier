47e087ba8eceb6b58f30297e8df0129b60bd5d2d
Utkarsh <utkarsh39@hotmail.com>
2019-05-08 13:42:41 -0400

    Refactoring

--- src/storage/write_ahead_log/log_manager.cpp
+++ src/storage/write_ahead_log/log_manager.cpp
@@ -25,11 +25,11 @@ void LogManager::Process() {
 }
 
 void LogManager::Flush() {
-  // Set the flag for the log writer thread to persist the buffers to disk
-  do_persist_ = true;
-  // Wait for the log writer thread to persist the logs
   {
     std::unique_lock<std::mutex> lock(persist_lock_);
+    // Set the flag for the log writer thread to persist the buffers to disk
+    do_persist_ = true;
+    // Wait for the log writer thread to persist the logs
     persist_cv_.wait(lock, [&] { return !do_persist_; });
   }
   // Execute the callbacks for the transactions that have been persisted
